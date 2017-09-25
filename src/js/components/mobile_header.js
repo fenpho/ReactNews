5e2e8c9b31510988d5f290d3bc76864d779bc9e9
fenpho <fenpho@163.com>
2017-09-26 00:03:30 +0800

    ReactNews v1.0

---
+++ src/js/components/mobile_header.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+import {Router, Route, Link, browserHistory} from 'react-router';
+import {
+  Menu,
+  Icon,
+  Tabs,
+  message,
+  Form,
+  Input,
+  Button,
+  CheckBox,
+  Modal
+} from 'antd';
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
+
+class MobileHeader extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      current: 'top',
+      modalVisible: false,
+      action: 'login',
+      hasLogined: false,
+      userNickName: '',
+      userid: 0
+    }
+  }
+  componentWillMount(){
+    if(localStorage.userid != ''){
+      this.setState({
+        hasLogined: true,
+        userid: localStorage.userid,
+        userNickName: localStorage.userNickName
+      });
+    }
+  }
+  login(){
+    this.setModalVisible(true);
+  }
+  setModalVisible(value) {
+    this.setState({modalVisible: value});
+  }
+  handleClick(e) {
+    if (e.key == 'register') {
+      this.setState({current: 'register'});
+      this.setModalVisible(true);
+    } else {
+      this.setState({current: e.key});
+    }
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    var myFetchOptions = {
+      method: 'GET'
+    }
+    var formData = this.props.form.getFieldsValue();
+    console.log(formData);
+    fetch("http://newsapi.gugujiankong.com/Handler.ashx?action="+this.state.action+"&username="+formData.userName+"&password="+formData.password+"&r_userName=" + formData.r_userName + "&r_password="+ formData.r_password + "&r_confirmPassword=" + formData.r_confirmPassword, myFetchOptions).then(response=>response.json()).then(json=>{
+      if(json){
+        this.setState({
+          userNickName: json.NickUserName,
+          userid: json.UserId
+        });
+        localStorage.userid = json.UserId;
+        localStorage.userNickName = json.NickUserName;
+      }else{
+        message.success("很抱歉，登录失败，请先注册！");
+        this.setState({
+          action: ''
+        });
+        console.log(this.state.action);
+      }
+      if(this.state.action=="login"){
+        this.setState({
+          hasLogined: true
+        });
+        message.success("恭喜您，登录成功！");
+      }else if(this.state.action=="register"){
+        message.success("恭喜您，注册成功！");
+      }
+    });
+    this.setModalVisible(false);
+  }
+  callback(key){
+    if(key=="1"){
+      this.setState({
+        action: "login"
+      });
+    }else{
+      this.setState({
+        action: "register"
+      });
+    }
+  }
+  logout(){
+    localStorage.userid = '';
+    localStorage.userNickName = '';
+    this.setState({
+      hasLogined: false
+    });
+  }
+  render() {
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+    const userShow = this.state.hasLogined ?
+    <Link to={`/usercenter`}>
+      <Icon type="inbox"/>
+    </Link>
+    :
+    <Icon type="setting" onClick={this.login.bind(this)}/>
+    return (
+      <div id="mobileheader">
+        <header>
+          <img src="./src/images/logo.png" alt="logo" />
+          <span>ReactNews</span>
+          {userShow}
+          <Modal title="用户中心" wrapClassName="virtical-center-modal" visible={this.state.modalVisible} onOk={() => this.setModalVisible(false)} onText="关闭" onCancel={() => this.setModalVisible(false)}>
+            <Tabs type="card" onChange={this.callback.bind(this)}>
+              <TabPane tab="登录" key="1">
+                <Form onSubmit={this.handleSubmit.bind(this)}>
+                  <FormItem label="账户">
+                    {getFieldDecorator('userName')(<Input placeholder="请输入您的账号" />)}
+                  </FormItem>
+                  <FormItem label="密码">
+                    {getFieldDecorator('password')(<Input type="password" placeholder="请输入您的密码" />)}
+                  </FormItem>
+                  <Button type="primary" htmlType="submit">登录</Button>
+                </Form>
+              </TabPane>
+              <TabPane tab="注册" key="2">
+                <Form onSubmit={this.handleSubmit.bind(this)}>
+                  <FormItem label="账户">
+                    {getFieldDecorator('r_userName')(<Input placeholder="请输入您的账号" />)}
+                  </FormItem>
+                  <FormItem label="密码">
+                    {getFieldDecorator('r_password')(<Input type="password" placeholder="请输入您的密码" />)}
+                  </FormItem>
+                  <FormItem label="密码">
+                    {getFieldDecorator('r_confirmPassword')(<Input type="password" placeholder="请输入您的密码" />)}
+                  </FormItem>
+                  <Button type="primary" htmlType="submit">注册</Button>
+                </Form>
+              </TabPane>
+            </Tabs>
+          </Modal>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default MobileHeader  = Form.create({})(MobileHeader);
---
+++ src/js/components/pc_newscontainer.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+import {Tabs, Carousel} from 'antd';
+import PCNewsBlock from './pc_news_block';
+import PCNewsImageBlock from './pc_news_image_block';
+import PCProduct from './pc_products';
+
+const TabPane = Tabs.TabPane;
+
+export default class PCNewsContainer extends React.Component{
+  render(){
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      autoplay: true
+    };
+    return(
+      <Row>
+        <Col span={2}></Col>
+        <Col span={20} class="container">
+          <div class="leftContainer">
+            <div class="carousel">
+              <Carousel {...settings}>
+                <div><img src="./src/images/carousel_1.jpg" /></div>
+                <div><img src="./src/images/carousel_2.jpg" /></div>
+                <div><img src="./src/images/carousel_3.jpg" /></div>
+                <div><img src="./src/images/carousel_4.jpg" /></div>
+              </Carousel>
+            </div>
+            <PCNewsImageBlock count={6} type="guoji" width="400px" cardTitle="国际头条" imageWidth="112px"/>
+          </div>
+          <Tabs class="tab_news">
+            <TabPane tab="头条" key="1">
+              <PCNewsBlock type="top" count={22} width="100%" bordered="false" />
+            </TabPane>
+            <TabPane tab="国际" key="2">
+              <PCNewsBlock type="guoji" count={22} width="100%" bordered="false" />
+            </TabPane>
+          </Tabs>
+          <Tabs class="tabs_product">
+            <TabPane tab="ReactNews 产品" key="1">
+              <PCProduct/>
+            </TabPane>
+          </Tabs>
+          <PCNewsImageBlock count={8} type="guonei" width="100%" cardTitle="国内头条" imageWidth="125px"/>
+          <PCNewsImageBlock count={16} type="yule" width="100%" cardTitle="娱乐头条" imageWidth="125px"/>
+        </Col>
+        <Col span={2}></Col>
+      </Row>
+    );
+  }
+}
